9d6939222e8f2336452bb7ddcee8b8d83521f1ee
Hodaka <2841006960@qq.com>
2025-06-12 00:37:42 +0800

    chore: 添加 logging-service、permission-service 和 user-service 模块- 新增三个服务模块：logging-service、permission-service 和 user-service- 为每个模块添加基本的目录结构和配置文件

---
+++ logging-service/src/test/java/com/shuzi/loggingservice/LoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuzi.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ permission-service/src/test/java/com/shuzi/permissionservice/PermissionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuzi.permissionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
